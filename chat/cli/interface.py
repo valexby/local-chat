7393a11c545d72529558511a3ad3cf8f124016ca
valexby <realnats@gmail.com>
2018-02-06 13:56:06 +0300

    Fix connection

--- chat/cli/interface.py
+++ chat/cli/interface.py
@@ -33,23 +33,7 @@ class ChatInterface(object):
     def __init__(self):
         self._messages = []
         self._connected_clients = set()
-        # self._main_loop = urwid.MainLoop(self._init_interface(), self._palette)
         self._cmd_dispatcher = CommandDispatcher(self)
-
-    def _init_interface(self):
-        """Returns main interface frame."""
-        header = urwid.LineBox(
-            urwid.Text('Local Chat by Yury Zaitsau, 2017', align=urwid.CENTER))
-        body = urwid.Columns([
-            (urwid.LineBox(urwid.ListBox(self._messages), title="Chat history")),
-            ('fixed', 20, urwid.LineBox(urwid.ListBox(self._connected_clients), title="Clients online")),
-        ])
-        edit = _PatchedEdit(caption=' command: ', multiline=True)
-        urwid.connect_signal(edit, 'command_entered', self._command_entered)
-        footer = urwid.LineBox(edit, title='Command line')
-        frame = urwid.Frame(body, header, footer)
-        frame.focus_position = 'footer'
-        return frame
 
     def _command_entered(self, edit, text):
         if not text.startswith('\\'):
@@ -59,8 +43,7 @@ class ChatInterface(object):
 
     def run(self):
         try:
-            # self._start_program_logics()
-            # self._main_loop.run()
+            self._start_program_logics()
             while True:
                 command = input()
                 if not command.startswith('\\'):
